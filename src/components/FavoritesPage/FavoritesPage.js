fe81262406ca9d73056599a68aa715275ee77ccb
Sem19 <semkivvolodya@gmail.com>
2024-05-22 14:29:25 +0200

    add some logic

--- src/components/FavoritesPage/FavoritesPage.js
+++ src/components/FavoritesPage/FavoritesPage.js
@@ -1,4 +1,8 @@
+import { useSelector } from "react-redux";
+
 const FavoritesPage = () => {
+  const listOfFavorites = useSelector((state) => state.favorite.listOfFavorites);
+  console.log(listOfFavorites);
   return <div>FavoritesPage</div>;
 };
 export default FavoritesPage;
