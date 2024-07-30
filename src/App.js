778fc1460e5c825dcbf2c44266e03ce2688eb8ca
Sem19 <semkivvolodya@gmail.com>
2024-07-30 18:18:29 +0200

    hash router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./reset.css";
 import "./index.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/header/header";
 import CatalogPage from "./components/catalog-page/catalog-page";
 import ErrorBoundary from "./components/Shared/error-boundary/error-boundary";
@@ -11,32 +11,34 @@ function App() {
   return (
     <div>
       <Header />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <ErrorBoundary>
-              <HomePage />
-            </ErrorBoundary>
-          }
-        />
-        <Route
-          path="/catalog"
-          element={
-            <ErrorBoundary>
-              <CatalogPage />
-            </ErrorBoundary>
-          }
-        />
-        <Route
-          path="/favorites"
-          element={
-            <ErrorBoundary>
-              <FavoritesPage />
-            </ErrorBoundary>
-          }
-        />
-      </Routes>
+      <HashRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ErrorBoundary>
+                <HomePage />
+              </ErrorBoundary>
+            }
+          />
+          <Route
+            path="/catalog"
+            element={
+              <ErrorBoundary>
+                <CatalogPage />
+              </ErrorBoundary>
+            }
+          />
+          <Route
+            path="/favorites"
+            element={
+              <ErrorBoundary>
+                <FavoritesPage />
+              </ErrorBoundary>
+            }
+          />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
