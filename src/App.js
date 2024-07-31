9d54d053f5b7458ff6423d0abebdae9896117813
Sem19 <semkivvolodya@gmail.com>
2024-07-31 10:07:54 +0200

    remove hashroutes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/header/header";
 import CatalogPage from "./components/catalog-page/catalog-page";
 import ErrorBoundary from "./components/Shared/error-boundary/error-boundary";
@@ -10,37 +9,32 @@ function App() {
   return (
     <div>
       <Header />
-      <HashRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <ErrorBoundary>
-                <HomePage />
-              </ErrorBoundary>
-            }
-            key="home"
-          />
-          <Route
-            path="/catalog"
-            element={
-              <ErrorBoundary>
-                <CatalogPage />
-              </ErrorBoundary>
-            }
-            key="catalog"
-          />
-          <Route
-            path="/favorites"
-            element={
-              <ErrorBoundary>
-                <FavoritesPage />
-              </ErrorBoundary>
-            }
-            key="favorites"
-          />
-        </Routes>
-      </HashRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ErrorBoundary>
+              <HomePage />
+            </ErrorBoundary>
+          }
+        />
+        <Route
+          path="/catalog"
+          element={
+            <ErrorBoundary>
+              <CatalogPage />
+            </ErrorBoundary>
+          }
+        />
+        <Route
+          path="/favorites"
+          element={
+            <ErrorBoundary>
+              <FavoritesPage />
+            </ErrorBoundary>
+          }
+        />
+      </Routes>
     </div>
   );
 }
